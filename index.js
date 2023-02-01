5e211155d876d2de5dc5e7392d78b8c41a4cd3a7
Jonas Larsson <jonas.alf.larsson@gmail.com>
2023-02-01 14:39:40 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import env from 'dotenv';
+import { Configuration, OpenAIApi } from 'openai';
+
+const app = express();
+env.config();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const configuration = new Configuration({
+    organization: process.env.OPEN_ORG_ID,
+    apiKey: process.env.OPEN_API_KEY
+});
+const openai = new OpenAIApi(configuration);
+
+const PORT = process.env.PORT || 9000;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+app.post("/", async (req, res) => {
+    const { message } = req.body;
+    try {
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `${message}`,
+            max_tokens: 100,
+            temperature: .5
+        });
+        res.json({ message: response.data.choices[0].text });
+    } catch (error) {
+        console.log(error);
+        res.send(error).status(400);
+    }
+});
